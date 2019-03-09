790b7e1cfbf77ea4236accc406df99eb11d61242
Marius Voinescu <marius.a.voinescu@gmail.com>
2019-03-09 09:30:48 +0200

    v.1.1
    - Added the Income/Expense function const
    - Added a new item to the budget controller

--- app.js
+++ app.js
@@ -2,7 +2,65 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
     
-    // To add later
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+        
+    };
+
+    return {
+        addItem: function(type, des, val){
+
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else{
+                ID = 0;
+            }
+            
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc'){
+                newItem = new Income(ID,des,val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function(){
+            console.log(data);
+        }
+    };
+
+
+
 })();
 
 
@@ -23,6 +81,15 @@ var UIController = (function(){
                 description: document.querySelector(DOMStrings.inputDocumentation).value,
                 value:document.querySelector(DOMStrings.inputValue).value
             };
+        },
+
+        addListITem: function(obj, type){
+            // Create HTML string with palceholder text
+
+            // Replace placeholder text with some data
+
+
+            // Insert the HTML inside the DOM
         },
 
 
@@ -59,13 +126,15 @@ var controller = (function(budgetCtrl, UICtrl){
     var controlAddItem = function(){
         // TODO - 
 
+        // Var declaration
+        var input, newItem;
         // 1. Get the filed input data
 
-        var input = UICtrl.getinput();
-        console.log(input);
-
+        input = UICtrl.getinput();
+        
         // 2. Add the item to the budget controller
 
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the tiem to the UI
 
         // 4. Calculate the budget
